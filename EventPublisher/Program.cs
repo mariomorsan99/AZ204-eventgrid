4990436ac51f2ffcbc36fd52a7bc752bf89064c9
unknown <mariomorsan99@gmail.com>
2021-04-08 14:04:51 -0700

    se sube app

---
+++ EventPublisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Azure;
+using Azure.Messaging.EventGrid;
+using System.Threading.Tasks;
+
+namespace EventPublisher
+{
+    class Program
+    {
+        private const string topicEndpoint = "https://hrtopics.eastus-1.eventgrid.azure.net/api/events";
+        private const string topicKey = "EF6BGA1UpGrLVO8d4D93LCqbufKRe+tb1SeiGOirnYg=";
+       
+
+        public static async Task Main(string[] args)
+            {
+                Uri endpoint = new Uri(topicEndpoint); 
+                AzureKeyCredential credential = new AzureKeyCredential(topicKey);
+                EventGridPublisherClient client = new EventGridPublisherClient(endpoint, credential);
+                
+                EventGridEvent firstEvent= new EventGridEvent(
+                    subject: $"New Employee: Alba Sutton",
+                    eventType: "Employees.Registration.New",
+                    dataVersion: "1.0",
+                         data: new
+                              {
+                                FullName = "Alba Sutton",
+                                 Address = "4567 Pine Avenue, Edison, WA 97202"
+                                }
+                );
+                
+                EventGridEvent secondEvent= new EventGridEvent(
+                  subject: $"New Employee: Alexandre Doyon",
+                  eventType: "Employees.Registration.New",
+                  dataVersion: "1.0",
+                  data: new
+                       {
+                                    FullName = "Alexandre Doyon",
+                                             Address = "456 College Street, Bow, WA 98107"
+                       
+                       }
+                );
+
+                await client.SendEventAsync(firstEvent);
+                Console.WriteLine("primer evnto publicado");
+
+                await client.SendEventAsync(secondEvent);
+                Console.WriteLine("segundo evento publicado");
+
+
+            }
+            
+    }
+}
